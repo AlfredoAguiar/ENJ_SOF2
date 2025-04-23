ffe66f9c0f671fccf44e26b52bb6bcc7ee11b4f0
alfredo <alfredoaguiar@ipvc.pt>
2025-04-23 10:53:32 +0100

    erros manage e prefil

---
+++ BlazorApp5/services/UserStateService.cs
@@ -0,0 +1,5 @@
+public class UserStateService
+{
+    // Change the type to Guid
+    public Guid UserId { get; set; }
+}
--- dto/obj/Debug/net8.0/dto.AssemblyInfo.cs
+++ dto/obj/Debug/net8.0/dto.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed5e897890c3fae1b849a80fd77530cf4b60a549")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb435428ce41aabf3175a077d25efc38a46a731e")]
 [assembly: System.Reflection.AssemblyProductAttribute("dto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
