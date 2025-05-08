6a6905e00313449026b085e9724de88bcc85eee1
alfredo <alfredoaguiar@ipvc.pt>
2025-05-08 16:25:42 +0100

    add

--- TestProject1/obj/Debug/net8.0/TestProject1.AssemblyInfo.cs
+++ TestProject1/obj/Debug/net8.0/TestProject1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb435428ce41aabf3175a077d25efc38a46a731e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+388b013c1c747d1d3c5cf934533f5a357a4b9420")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
